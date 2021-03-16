efb41d15276f010c5c52a60633cad44e57b453a7
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2021-03-16 12:35:01 +0000

    sudoku_rows

--- ai-f20-m4/6. Computer Vision/07. Template matching, corners and Haar cascades/sudoko_solver.py
+++ ai-f20-m4/6. Computer Vision/07. Template matching, corners and Haar cascades/sudoko_solver.py
@@ -1,5 +1,6 @@
 import cv2
 import imutils
+from imutils import contours as cts
 import numpy as np
 import os
 os.environ['DISPLAY'] = ':0'
@@ -13,17 +14,41 @@ def print_image(image):
 image = cv2.imread('6. Computer Vision/07. Template matching, corners and Haar cascades/img/sudoku.jpg', 0)
 #gray = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
 thresh = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 57, 5)
-#print(thresh.shape)
+print(thresh.shape)
 contourns, h = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 
 for contourn in contourns:
     area = cv2.contourArea(contourn)
-    if area < 500:
-        cv2.drawContours(thresh, [contourn], -1, (0,0,0), -1)
+    if area < 550:
+        cv2.drawContours(thresh, [contourn], -1, (0,255,0), -1)
+
 
 vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,5))
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=9)
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, vertical_kernel, iterations=5)
 horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=4) 
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, horizontal_kernel, iterations=5) 
 
-print_image(thresh)
+#print_image(thresh)
+invert = 255 - thresh
+#print_image(invert)
+contourns, h = cv2.findContours(invert, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#cv2.drawContours(invert, contourns, -1, (0,255,0), 2)
+#print_image(invert)
+#print("Here", len(contourns))
+#print("Here", invert)
+contourns, _ = cts.sort_contours(contourns, method = 'top-to-bottom')
+temp_row = []
+sudoku_rows = []
+for (i, c) in enumerate(contourns, 1):
+    area = cv2.contourArea(c)
+    if area < 4500:
+        temp_row.append(c)
+        if i % 9 == 0:
+            contourns, _ = cts.sort_contours(temp_row, method = "left-to-right")
+            sudoku_rows.append(contourns)
+            row = []
+print(len(sudoku_rows))
+
+
+#print_image(invert)
